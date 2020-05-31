dd02bb0468e7da3c41d3b6fe20d3e77c097029a7
Jorge Garcia <jorgegar896@gmail.com>
2020-05-31 13:45:50 -0600

    Commit correspondiente al patrón de diseño Factory, basado en la creación de jugadores para el juego FIFA

---
+++ FifaFactory/Forward.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FifaFactory;
+
+/**
+ *
+ * @author jorge
+ */
+public class Forward extends Player{
+    
+    public Forward(String name){
+        super(PlayerPosition.FORWARD, name);
+    }
+    
+}
